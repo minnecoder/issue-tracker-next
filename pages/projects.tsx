c09a99ca8a74ceba0b954310c5621df59a4df21b
Sean Parkin <sdparkin@gmail.com>
2024-07-18 00:30:47 -0500

    Created projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { NextPage } from "next";
 import Header from "../components/Header/Header";
-import Sidebar from "../components/Sidebar";
-import ProjectsList from "../components/ProjectsList";
+import Sidebar from "../components/Sidebar/Sidebar";
+import ProjectsList from "../components/ProjectsList/ProjectsList";
 import styles from "../styles/Projects.module.css";
 
 const Projects: NextPage = () => {
